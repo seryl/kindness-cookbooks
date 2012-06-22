b1e59d007ddedd8a51742a9e324808c1c2e01cec
Josh Toft <joshtoft@gmail.com>
2012-06-22 00:25:38 -0700

    Adding treetop and rinari

---
+++ emacs_config/recipes/rinari.rb
@@ -0,0 +1,31 @@
+#
+# Author:: Josh Toft <joshtoft@gmail.com>
+# Cookbook Name:: emacs_config
+# Recipe:: rinari
+#
+# Copyright 2012, Josh Toft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "emacs_config::treetop"
+
+template "~/.emacs.d/#{ENV['USER']}/rinari.el" do
+  source "rinari.el.erb"
+end
+
+vendor_module "rinari" do
+  type "git"
+  source "https://github.com/eschulte/rinari.git"
+  append false
+end
