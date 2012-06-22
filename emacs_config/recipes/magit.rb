d4a9f50463d7f2af0597c37d074d853d5bb20397
Josh Toft <joshtoft@gmail.com>
2012-06-22 16:02:54 -0700

    Adding magit to emacs config

---
+++ emacs_config/recipes/magit.rb
@@ -0,0 +1,24 @@
+#
+# Author:: Josh Toft <joshtoft@gmail.com>
+# Cookbook Name:: emacs_config
+# Recipe:: magit
+#
+# Copyright 2012, Josh Toft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+vendor_module "magit" do
+  type "git"
+  source "https://github.com/technomancy/magit.git"
+end
