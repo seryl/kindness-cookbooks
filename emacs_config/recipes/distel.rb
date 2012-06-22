dcf13888e04f544153f9ae8dab09d896a2cf8117
Josh Toft <joshtoft@gmail.com>
2012-06-22 10:32:11 -0700

    more distel work

--- emacs_config/recipes/distel.rb
+++ emacs_config/recipes/distel.rb
@@ -33,7 +33,6 @@ bash "setup distel.el" do
   code <<-EOH
   cd #{ENV['HOME']}/.emacs.d/vendor/distel.el
   make
-  make install
   EOH
-  not_if { ::File.exists?("/usr/local/share/distel/elisp") }
+  # not_if { ::File.exists?("#{ENV['HOME']}/.emacs.d/vendor/distel.el/elisp") }
 end
